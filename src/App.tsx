1bfe3b3e426c9574057f1cf928e877566e99c745
aion0721 <rudolf.aion@gmail.com>
2022-10-11 22:23:56 +0900

    Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider, Button } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">Button</Button>
+      <p>aaa</p>
     </ChakraProvider>
   );
 }
