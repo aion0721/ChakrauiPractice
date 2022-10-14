bb0477a0c6725468d666ffa65cbb955a59b4dfde
aion0721 <rudolf.aion@gmail.com>
2022-10-14 23:12:44 +0900

    Login function

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,18 @@
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, VFC } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const { login, loading } = useAuth();
+  const [userId, setUserId] = useState("");
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+  const onClickLogin = () => {
+    login(userId);
+  };
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,8 +21,18 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="UserId" />
-          <PrimaryButton>Login</PrimaryButton>
+          <Input
+            placeholder="UserId"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            onClick={onClickLogin}
+            loading={loading}
+            disabled={userId === ""}
+          >
+            Login
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
